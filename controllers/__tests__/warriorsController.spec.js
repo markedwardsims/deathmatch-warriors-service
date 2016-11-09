6156eba4dee2d09fb530b1bbf871e5fac51e6c38
Mark Sims <markedwardsims@gmail.com>
2016-11-08 22:12:41 -0600

    adding placeholder test file for warriors controller

---
+++ controllers/__tests__/warriorsController.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+import WarriorsController, {__RewireAPI__ as WarriorsControllerRewire} from '../warriorsController.js';
+
+describe('the Warriors Controller', () => {
+
+    it('should get all warriors', () => {});
+
+    it('should get a single warrior by id');
+
+    it('should update a warrior');
+
+});
